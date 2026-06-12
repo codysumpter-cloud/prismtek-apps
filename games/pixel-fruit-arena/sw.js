6d99c2dd9f22ed1afe7450b024d29413692e3c89
Prismtek <cody.sumpter@gmail.com>
2026-06-12 11:12:35 -0400

    Add Pixel Fruit Arena offline service worker

---
+++ games/pixel-fruit-arena/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "pixel-fruit-arena-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./src/main.js",
+  "./src/fruits/fruits.js",
+  "./src/stages/skyRuins.js",
+  "./src/characters/characterCreator.js",
+  "./src/systems/matchSystem.js",
+  "./src/systems/runtimeConfig.js",
+  "./src/combat/combatSystem.js",
+  "./src/multiplayer/input.js",
+  "./src/ui/dom.js",
+  "./src/ui/renderer.js",
+  "./src/ui/styles.css",
+  "./data/fruits/fruits.json",
+  "./data/stages/sky_ruins.json",
+  "./assets/characters/prismtek_placeholder.svg",
+  "./assets/characters/prismtek_placeholder_character.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
